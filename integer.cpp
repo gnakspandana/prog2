a1c3f3904738adcc74a32abf0e300a2da7f73638
gnakspandana <93626896+gnakspandana@users.noreply.github.com>
2021-11-03 03:35:16 +0100

    Update integer.cpp

--- integer.cpp
+++ integer.cpp
@@ -6,25 +6,18 @@ class Integer{
 		Integer(int);
 		int get();
 		void set(int);
-		int a,b,c;
+		
+		int fib();
 	private:
 		int val;
+		int fibp(int);
 	};
  
 Integer::Integer(int n){
 	val = n;
-	a=0;
-	b=1;
-	cout <<a<<" "<<b;
-	//using loop for fibonacci
-	for (int i=1;i<=n-2;i++){
-	//adding pervious terms to get the next term
-    	c=a+b;
-    	cout <<" "<<c;
-    	a=b;
-    	b=c;
-    	}
 	}
+
+
 
 int Integer::get(){
 	return val;
@@ -34,10 +27,28 @@ void Integer::set(int n){
 	val = n;
 	}
 
+int Integer::fib(){
+    return fibp(val);
+}
+
+int Integer::fibp(int n){
+    if(n==0){
+         return 0;
+         }
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 1;
+        }
+        return fibp(n-1)+fibp(n-2);
+    }
+
 
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
+	int Integer fibp(Integer* integer) {return integer->fib()};
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
 		if (integer){
@@ -46,3 +57,4 @@ extern "C"{
 			}
 		}
 	}
+	
